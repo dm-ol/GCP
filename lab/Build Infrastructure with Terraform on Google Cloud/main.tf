27d59f112c51924f2fe780a2fac5741724cc748f
Dmytro Oliinyk <149588780+dm-ol@users.noreply.github.com>
2024-03-30 23:22:11 +0200

    Add files via upload

---
+++ lab/Build Infrastructure with Terraform on Google Cloud/main.tf
@@ -0,0 +1,71 @@
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-bucket-661359"
+ prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.53.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+
+  zone        = var.zone
+}
+
+module "instances" {
+
+  source     = "./modules/instances"
+
+}
+
+module "storage" {
+  source     = "./modules/storage"
+}
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 6.0.0"
+
+    project_id   = "qwiklabs-gcp-03-d48deb6f5539"
+    network_name = "tf-vpc-204043"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-central1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-central1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "Please like share & subscribe to quicklab"
+        },
+    ]
+}
+
+
+
+resource "google_compute_firewall" "tf-firewall"{
+  name    = "tf-firewall"
+ network = "projects/qwiklabs-gcp-03-d48deb6f5539/global/networks/tf-vpc-204043"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_tags = ["web"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
---
+++ lab/Build Infrastructure with Terraform on Google Cloud/variables.tf
@@ -0,0 +1,12 @@
+
+variable "region" {
+ default = "us-central1"
+}
+
+variable "zone" {
+ default = "us-central1-a"
+}
+
+variable "project_id" {
+ default = "qwiklabs-gcp-03-d48deb6f5539"
+}
